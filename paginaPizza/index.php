b18a82c052b6528b372cd55946a115c589399482
kiimot <kimlopezmoreno@gmail.com>
2022-03-02 20:50:07 +0100

    Cambios 02-03-2022

--- paginaPizza/index.php
+++ paginaPizza/index.php
@@ -11,9 +11,21 @@
         <!-- codigo web principal -->
         <!-- recepcion de datos -->
         <?php
+        session_start();
+        //Creamos la variable de sesion si no existe
+        
+        if (!isset($_SESSION['pizzas'])){
+            $_SESSION['pizzas'] = [] ;
+        }
+        else{
+            // Miramos si nos ha llegado una pizza seleccionada por parametro
             if (isset($_POST['pizza'])){
-                echo 'Has seleccionado '. $_POST['pizza'];
+                $pizzaSel = $listaPizzas[$_POST['pizza']];
+                //Añadimos la pizza a la variable sesion
+                $_SESSION['pizzas'][]= $pizzaSel;
             }
+        }
+
         ?>
         <table>
             <tr>
@@ -43,6 +55,42 @@
                 </tr>
             <?php } ?>
         </table>
+
+        <br>
+        <p>Pizza seleccionadas</p>
+        <?php
+        if (!empty($_SESSION['pizzas'])){?>
+            <table>
+            <tr>
+                <th></th>
+                <th>Nombre</th>
+                <th>Ingredientes</th>
+                <th>Masa</th>
+                <th>Especial</th>
+                <th>Precio</th>
+                <th>Cantidad</th>
+            </tr>
+            <?php
+            foreach ($_SESSION['pizzas'] as $pizza ){ ?>
+                <tr>
+                    <td><?= $pizza->getImg(); ?></td>
+                    <td><?= $pizza->getNombre(); ?></td>
+                    <td><?= implode(", ", $pizza->getIngredientes()); ?></td>
+                    <td><?= $pizza->getMasa(); ?></td>
+                    <td><?= $pizza->getEspecial(); ?></td>
+                    <td><?= $pizza->getPrecio(); ?></td>
+                    <td><input type="number" name="cantidad"></td>
+                    <td>
+                        <form action='./index.php' method='post'>
+                            <input type="hidden" name="pizza" value=<?= $pizza->getNombre();?> >
+                            <input type="submit" value="Añadir">
+                            <input type="submit" value="Eliminar">
+                        </form>
+                    </td>
+                </tr>
+            <?php } ?>
+        </table>
+        <?php }?>
         <!-- include footer -->
     </body>
 </html>
